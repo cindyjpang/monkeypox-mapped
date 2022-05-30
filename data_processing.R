8a989466183173f42378fa01a43a4b22630953b0
Cindy Pang <77863668+pangcind@users.noreply.github.com>
2022-05-29 22:42:31 -0400

    add code

--- data_processing.R
+++ data_processing.R
@@ -1,6 +1,16 @@
+library(plyr)
 library(readr)
 library(dplyr)
 library(plotly)
-library(sf)
+library(tidyr)
 
 gh_data <- read.csv("https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv")
+
+country_dat <- gh_data %>%
+  filter(Status == 'confirmed')%>%
+  dplyr::count(Status, Date_confirmation, Country)%>%
+  spread(Status, n)
+
+
+  
+  
