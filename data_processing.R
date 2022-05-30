fa67c2a48b7311d59fe0aa2b009b2c8d2db3b569
Cindy Pang <77863668+pangcind@users.noreply.github.com>
2022-05-29 22:45:56 -0400

    cp,,ot

--- data_processing.R
+++ data_processing.R
@@ -3,13 +3,17 @@ library(readr)
 library(dplyr)
 library(plotly)
 library(tidyr)
+library(sf)
 
 gh_data <- read.csv("https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv")
 
-country_dat <- gh_data %>%
+countries_sf <- read_sf("C:\\Users\\Cindy Pang\\monkeypox-mapped\\countries sf\\World_Countries__Generalized_.shp")
+
+country_confirmed_case_dat <- gh_data %>%
   filter(Status == 'confirmed')%>%
   dplyr::count(Status, Date_confirmation, Country)%>%
   spread(Status, n)
+
 
 
   
