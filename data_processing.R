6c5dfd80ed1ab5947b624f09ef88167ec91c102b
Cindy Pang <77863668+pangcind@users.noreply.github.com>
2022-06-04 16:10:53 -0400

    update code

--- data_processing.R
+++ data_processing.R
@@ -4,17 +4,50 @@ library(dplyr)
 library(plotly)
 library(tidyr)
 library(sf)
-
+library(stats)
+library(zoo)
 gh_data <- read.csv("https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv")
 
-countries_sf <- read_sf("C:\\Users\\Cindy Pang\\monkeypox-mapped\\shapefiles2\\ne_10m_admin_0_countries.shp")
+countries_sf <- read_sf("C:\\Users\\Cindy Pang\\monkeypox-mapped\\countries sf\\World_Countries__Generalized_.shp")
+##great_britain_sf <- read_sf("C:\\Users\\Cindy Pang\\monkeypox-mapped\\countries sf\\CTRY_DEC_2021_UK_BFC.shp")
+
 
 country_confirmed_case_dat <- gh_data %>%
   filter(Status == 'confirmed')%>%
   dplyr::count(Status, Date_confirmation, Country)%>%
-  spread(Status, n)
+  spread(Status, n)%>%
+  mutate(cumulative_confirmed = ave(confirmed, Country, FUN=cumsum))%>%
+  arrange(Country, .by_group = TRUE)
 
+# aggregate all the UK regions into one country
+uk_confirmed_case_dat <- country_confirmed_case_dat %>%
+  filter(Country == "England" | Country == "Wales" | Country == "Northern Ireland"| Country == "Scotland" | Country == "United Kingdom")%>%
+  group_by(Date_confirmation)%>%
+  summarize(United_Kingdom = sum(confirmed))%>%
+  gather(key = "Country", value = "confirmed", United_Kingdom)%>%
+  mutate(cumulative_confirmed = ave(confirmed, Country, FUN=cumsum))
 
+# aggregate all of the world data
+world_confirmed_case_dat <- country_confirmed_case_dat %>%
+  group_by(Date_confirmation)%>%
+  summarize(World = sum(confirmed))%>%
+  gather(key = "Country", value = "confirmed", World)%>%
+  mutate(cumulative_confirmed = ave(confirmed, Country, FUN=cumsum))
 
+countries_all <- rbind(country_confirmed_case_dat, uk_confirmed_case_dat, world_confirmed_case_dat)%>%
+  subset(Country != "England" & Country != "Wales" & Country != "Northern Ireland" & Country != "Scotland" & Country != "United Kingdom")%>%
+  group_by(Country)%>%
+  mutate(case_03d = rollmean(confirmed, k = 3, fill = NA, align = 'center'))
   
+
+
+
+# view by country plot
+
+country_cumulative <- plot_ly(countries_all, x = ~Date_confirmation, y = ~cumulative_confirmed, type = 'scatter',mode = "lines", color = ~Country)
+country_cumulative 
+
+country_daily <- plot_ly(countries_all, x = ~Date_confirmation, y = ~confirmed, type = 'bar',mode = "lines", color = ~Country)%>%
+  add_trace(y = ~case_03d, type = 'scatter', mode = "lines")
+country_daily
   
