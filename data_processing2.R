73bd131cd58d8f3f51172f57938a4092b7800425
Cindy Pang <77863668+pangcind@users.noreply.github.com>
2022-06-12 18:32:53 -0400

    6-12 update

--- data_processing2.R
+++ data_processing2.R
@@ -20,14 +20,34 @@ class(world_pop$pop2022)
 
 
 country_confirmed_case_dat <- gh_data %>%
-  dplyr::count(Status, Date_confirmation, Country)
-  # spread(Status, n)%>%
-  # mutate(cumulative_confirmed = ave(confirmed, Country, FUN=cumsum))%>%
-  # arrange(Country, .by_group = TRUE)
-
-hospitalizations <- gh_data %>%
+  mutate(Country = ifelse(Country == "England" | Country == "Wales" | Country == "Northern Ireland"| Country == "Scotland" | Country == "United Kingdom","United Kingdom", Country ),
+         Date_confirmation = ifelse(Status == "suspected", Date_entry, Date_confirmation))%>%
   filter(Status == "confirmed" | Status == "suspected")%>%
-  select(Status, Country, Date_confirmation, Hospitalised..Y.N.NA., Date_hospitalisation, Date_entry, Date_last_modified)%>%
-  mutate(Date_hospitalisation = ifelse(Date_hospitalisation == '', Date_entry, Date_hospitalisation))%>%
-  dplyr::count(Hospitalised..Y.N.NA., Date_hospitalisation, Country)%>%
-  filter(Hospitalised..Y.N.NA. == "Y")
+  dplyr::count(Status, Date_confirmation, Country)%>%
+  spread(Status, n)
+
+# aggregate all of the world data
+world_confirmed_case_dat <- country_confirmed_case_dat %>%
+  group_by(Date_confirmation)%>%
+  summarize(confirmed = sum(confirmed, na.rm = TRUE), 
+            suspected = sum(suspected, na.rm = TRUE))%>%
+  mutate(Country = "World")%>%
+  relocate(Country, .after = Date_confirmation)
+
+countries_all <-  rbind(country_confirmed_case_dat, world_confirmed_case_dat) %>%
+  group_by(Country)%>%
+  mutate(cumulative_confirmed = ave(confirmed, Country, FUN=cumsum),
+         cumulative_suspected = ave(suspected, Country, FUN=cumsum))
+
+
+
+
+# %>%
+
+
+# hospitalizations <- gh_data %>%
+#   filter(Status == "confirmed" | Status == "suspected")%>%
+#   select(Status, Country, Date_confirmation, Hospitalised..Y.N.NA., Date_hospitalisation, Date_entry, Date_last_modified)%>%
+#   mutate(Date_hospitalisation = ifelse(Date_hospitalisation == '', Date_entry, Date_hospitalisation))%>%
+#   dplyr::count(Hospitalised..Y.N.NA., Date_hospitalisation, Country)%>%
+#   filter(Hospitalised..Y.N.NA. == "Y")
